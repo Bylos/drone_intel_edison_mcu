d914d0e479901e3dee127fc74c90b77a67e9687e
Bylos <bylos72@gmail.com>
2016-01-08 19:54:01 +0100

    Put back data sending to CPU although ttymcu0 bug is not corrected by Intel for now

--- src/modes/mode_alexkidd.c
+++ src/modes/mode_alexkidd.c
@@ -19,7 +19,10 @@ mcu_mode_t Mode_AlexKidd_Run(cpu_mode_t cpu_mode) {
 	mcu_mode_t next_mode = MCU_MODE_ALEXKIDD;
 	/* Check for new sensors values */
 	if (lsm_get_inertial_flag()) {
-		/* Do nothing */
+		/* Send to CPU */
+		if (cpu_mode != CPU_MODE_UNKNOWN) {
+			cpu_send_inertial(lsm_get_inertial_data());
+		}
 	}
 	/* Check for new joystick values */
 	if (xbee_get_joystick_flag()) {
@@ -50,10 +53,15 @@ mcu_mode_t Mode_AlexKidd_Run(cpu_mode_t cpu_mode) {
 				pwm[1] + PCA_ONE_SHOT_1526_HZ_OFFSET,
 				pwm[2] + PCA_ONE_SHOT_1526_HZ_OFFSET,
 				pwm[3] + PCA_ONE_SHOT_1526_HZ_OFFSET);
+		/* Send to CPU */
+		if (cpu_mode != CPU_MODE_UNKNOWN) {
+			cpu_send_inertial(lsm_get_inertial_data());
+		}
 	}
 	/* Check for new esc values */
 	if (cpu_get_esc_pwm_flag()) {
-		/* Do nothing */
+		/* Do nothing but clear data */
+		cpu_get_esc_pwm_data();
 	}
 	/* Check for mode request */
 	if (xbee_get_command_flag()) {
--- src/modes/mode_unarmed.c
+++ src/modes/mode_unarmed.c
@@ -12,26 +12,33 @@
  * UNARMED MODE
  *******************************************************************************/
 void Mode_Unarmed_Init(void) {
-	debug_print(DBG_INFO, " ---> Entering Unarmed State\n");
 	pca_stop();
 	lsm_clear_flags();
 	xbee_clear_flags();
 	cpu_clear_flags();
+	debug_print(DBG_INFO, " ---> Entering Unarmed State\n");
 }
 
 mcu_mode_t Mode_Unarmed_Run(cpu_mode_t cpu_mode) {
 	mcu_mode_t next_mode = MCU_MODE_UNARMED;
 	/* Check for new sensors values */
 	if (lsm_get_inertial_flag()) {
-		/* Do nothing */
+		/* Send to CPU */
+		if (cpu_mode != CPU_MODE_UNKNOWN) {
+			cpu_send_inertial(lsm_get_inertial_data());
+		}
 	}
 	/* Check for new joystick values */
 	if (xbee_get_joystick_flag()) {
-		/* Do nothing */
+		/* Send to CPU */
+		if (cpu_mode != CPU_MODE_UNKNOWN) {
+			cpu_send_joystick(xbee_get_joystick_data());
+		}
 	}
 	/* Check for new esc values */
 	if (cpu_get_esc_pwm_flag()) {
-		/* Do nothing */
+		/* Do nothing but clear data */
+		cpu_get_esc_pwm_data();
 	}
 	/* Check for mode request */
 	if (xbee_get_command_flag()) {
