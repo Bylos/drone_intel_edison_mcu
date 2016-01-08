cc709a0a3aeaf6d008a513a47cfd4fdc30e1c794
Bylos <bylos72@gmail.com>
2016-01-08 19:52:47 +0100

    Remove improper ternary operators leading to fatal task error - replaced by 'if' statements

--- src/modes/mode_alexkidd.c
+++ src/modes/mode_alexkidd.c
@@ -23,27 +23,28 @@ mcu_mode_t Mode_AlexKidd_Run(cpu_mode_t cpu_mode) {
 	}
 	/* Check for new joystick values */
 	if (xbee_get_joystick_flag()) {
-		/* Get them */
+		// Get them
 		joystick_t joystick = xbee_get_joystick_data();
-		/* Then play with motors */
-		int front_diff  = ((int)(joystick.left.y)  * 27) >> (6+3); // pitch limited to 12.5%
-		int sides_diff  = ((int)(joystick.left.x)  * 27) >> (6+3); // roll limited to 12.5%
+		// Then play with motors
+		int front_diff  = (joystick.left.y  * 27) >> (6+3); // pitch limited to 12.5%
+		int sides_diff  = (joystick.left.x  * 27) >> (6+3); // roll limited to 12.5%
 		int pwm[4] = {
 				front_diff - sides_diff,	// front left motor
 				front_diff + sides_diff,	// front right motor
 				- front_diff + sides_diff,  // rear right motor
 				- front_diff - sides_diff	// rear left motor
 		};
-		/* Just verify pwm range and limit max to 25% per motor */
-		pwm[0] = pwm[0] < 0 ? 0 : pwm[0];
-		pwm[0] = pwm[0] > (PCA_ONE_SHOT_1526_HZ_RANGE >> 2) ? PCA_ONE_SHOT_1526_HZ_RANGE >> 2 : pwm[0];
-		pwm[1] = pwm[1] < 0 ? 0 : pwm[1];
-		pwm[1] = pwm[1] > (PCA_ONE_SHOT_1526_HZ_RANGE >> 2) ? PCA_ONE_SHOT_1526_HZ_RANGE >> 2 : pwm[1];
-		pwm[2] = pwm[2] < 0 ? 0 : pwm[2];
-		pwm[2] = pwm[2] > (PCA_ONE_SHOT_1526_HZ_RANGE >> 2) ? PCA_ONE_SHOT_1526_HZ_RANGE >> 2 : pwm[2];
-		pwm[3] = pwm[3] < 0 ? 0 : pwm[3];
-		pwm[3] = pwm[3] > (PCA_ONE_SHOT_1526_HZ_RANGE >> 2) ? PCA_ONE_SHOT_1526_HZ_RANGE >> 2 : pwm[3];
-		/* Then put that to pwm driver ! */
+		// Just verify pwm range and limit max to 25% per motor
+		int pwm_max = PCA_ONE_SHOT_1526_HZ_RANGE >> 2;
+		if(pwm[0] < 0) pwm[0] = 0;
+		if(pwm[0] > pwm_max) pwm[0] = pwm_max;
+		if(pwm[1] < 0) pwm[1] = 0;
+		if(pwm[1] > pwm_max) pwm[1] = pwm_max;
+		if(pwm[2] < 0) pwm[2] = 0;
+		if(pwm[2] > pwm_max) pwm[2] = pwm_max;
+		if(pwm[3] < 0) pwm[3] = 0;
+		if(pwm[3] > pwm_max) pwm[3] = pwm_max;
+		// Then put that to pwm driver !
 		pca_channel_0_3(
 				pwm[0] + PCA_ONE_SHOT_1526_HZ_OFFSET,
 				pwm[1] + PCA_ONE_SHOT_1526_HZ_OFFSET,
